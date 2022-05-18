f07298d21ab442e68c24e12f00faf705d46eae7f
reuschj <reuschj@gmail.com>
2022-05-17 23:47:56 -0500

    update deps

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import TimeAmount from "./src/TimeAmount";
-import TimeUnit, { getConversionValue, getTimeUnitPosition, getTimeUnitFromPosition, getPreviousTimeUnit, getNextTimeUnit } from "./src/TimeUnit";
-import TimeDescriptionSetup, { TimeDescriptionTemplateCreator, defaultTemplateCreator } from "./src/TimeDescriptionSetup";
-export { TimeAmount, TimeUnit, TimeDescriptionSetup, getConversionValue, getTimeUnitPosition, getTimeUnitFromPosition, getPreviousTimeUnit, getNextTimeUnit, TimeDescriptionTemplateCreator, defaultTemplateCreator };
